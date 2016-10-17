d314ae893bdb9cd6b457c4fbda1e122538e2a5aa
ottagit <ottachris13@yahoo.com>
2016-10-17 16:17:11 +0300

    many urls sharing a handler

--- server.rb
+++ server.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
 
-get '/' do
-  "Hello, world!"
+['/one', '/two', 'three', '/four', '/five', '/six'].each do |route|
+
+ get route do
+  "Triggered via #{route}"
+ end
+
+ post route do
+  "Triggered via #{route}"
+ end
+
+ put route do
+  "Triggered via #{route}"
+ end
+
+ delete route do
+  "Triggered via #{route}"
+ end
+
+ patch route do
+  "Triggered via #{route}"
+ end
+
+ options route do
+  "Triggered via #{route}"
+end
 end
